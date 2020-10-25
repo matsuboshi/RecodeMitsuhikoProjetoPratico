652bb7224bb8b0cc90b449afa2e62723d4353898
matsuboshi <matsuboshi@hotmail.com>
2020-10-25 13:18:53 -0300

    Inserting extra queries to complete 10 items

--- 04 - Intro to DB (SQL)/queryPedidos.sql
+++ 04 - Intro to DB (SQL)/queryPedidos.sql
@@ -2,9 +2,11 @@
 -- DROP TABLE fseletro.pedidos;
 -- DROP SCHEMA fseletro;
 -- -- DANGER ZONE -- --
-
 CREATE SCHEMA fseletro;
+
+
 USE fseletro;
+
 
 CREATE TABLE fseletro.pedidos (
 	id INT AUTO_INCREMENT NOT NULL,
@@ -12,62 +14,250 @@ CREATE TABLE fseletro.pedidos (
 	cliente_nome VARCHAR(500) NOT NULL,
 	cliente_endereco VARCHAR(500) NOT NULL,
 	cliente_telefone VARCHAR(15) NOT NULL,
-	id_produto INT NOT NULL, 
-	preco_unitario DECIMAL(8,2),
+	id_produto INT NOT NULL,
+	preco_unitario DECIMAL(8, 2),
 	quantidade INT NOT NULL,
-	valor_total DECIMAL(10,2) AS (preco_unitario*quantidade),
+	valor_total DECIMAL(10, 2) AS (preco_unitario * quantidade),
 	PRIMARY KEY (id)
 );
 
 
-INSERT INTO pedidos (cliente_nome, cliente_endereco, cliente_telefone, quantidade, preco_unitario, id_produto)
-SELECT "Jane Smith", "R. das Flores, 785, Vila Madalena - Sao Paulo, SP","+551112342007", 1, pr.preco_venda, 4
-FROM produtos pr
-WHERE pr.id = 4; 
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Jane Smith",
+	"R. das Flores, 785, Vila Madalena - Sao Paulo, SP",
+	"+551112342007",
+	1,
+	pr.preco_venda,
+	4
+FROM
+	produtos pr
+WHERE
+	pr.id = 4;
 
-INSERT INTO pedidos (cliente_nome, cliente_endereco, cliente_telefone, quantidade, preco_unitario, id_produto)
-SELECT "Mohammed Ranjit", "Av. Paulista, 515, Jardins - Sao Paulo, SP","+551196335678", 3, pr.preco_venda, 3
-FROM produtos pr
-WHERE pr.id = 3; 
 
-INSERT INTO pedidos (cliente_nome, cliente_endereco, cliente_telefone, quantidade, preco_unitario, id_produto)
-SELECT "Juan Dolores", "R. Cabedelo, 224, Vila Sonia - Sao Paulo, SP","+551196909090", 1, pr.preco_venda, 7
-FROM produtos pr
-WHERE pr.id = 7; 
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Mohammed Ranjit",
+	"Av. Paulista, 515, Jardins - Sao Paulo, SP",
+	"+551196335678",
+	3,
+	pr.preco_venda,
+	3
+FROM
+	produtos pr
+WHERE
+	pr.id = 3;
 
-INSERT INTO pedidos (cliente_nome, cliente_endereco, cliente_telefone, quantidade, preco_unitario, id_produto)
-SELECT "Mitsuo Hashida", "R. Pedroso Alvarenga, 725, Itaim Bibi - Sao Paulo, SP","+551196336611", 4, pr.preco_venda, 1
-FROM produtos pr
-WHERE pr.id = 1; 
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Juan Dolores",
+	"R. Cabedelo, 224, Vila Sonia - Sao Paulo, SP",
+	"+551196909090",
+	1,
+	pr.preco_venda,
+	7
+FROM
+	produtos pr
+WHERE
+	pr.id = 7;
 
 
-INSERT INTO pedidos (cliente_nome, cliente_endereco, cliente_telefone, quantidade, preco_unitario, id_produto)
-SELECT "Jake Portman", "R. Frei Caneca, 640, Consolação - Sao Paulo, SP","+551145672211", 1, pr.preco_venda, 2
-FROM produtos pr
-WHERE pr.id = 2; 
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Mitsuo Hashida",
+	"R. Pedroso Alvarenga, 725, Itaim Bibi - Sao Paulo, SP",
+	"+551196336611",
+	4,
+	pr.preco_venda,
+	1
+FROM
+	produtos pr
+WHERE
+	pr.id = 1;
 
-INSERT INTO pedidos (cliente_nome, cliente_endereco, cliente_telefone, quantidade, preco_unitario, id_produto)
-SELECT "Olga Petrolenko", "R. Suíça, 406, Pinheiros - Sao Paulo, SP","+551196354311", 2, pr.preco_venda, 3
-FROM produtos pr
-WHERE pr.id = 3; 
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Jake Portman",
+	"R. Frei Caneca, 640, Consolação - Sao Paulo, SP",
+	"+551145672211",
+	1,
+	pr.preco_venda,
+	2
+FROM
+	produtos pr
+WHERE
+	pr.id = 2;
+
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Olga Petrolenko",
+	"R. Suíça, 406, Pinheiros - Sao Paulo, SP",
+	"+551196354311",
+	2,
+	pr.preco_venda,
+	3
+FROM
+	produtos pr
+WHERE
+	pr.id = 3;
+
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Jake Russell",
+	"R. Frei Caneca, 640, Consolação - Sao Paulo, SP",
+	"+551145672211",
+	1,
+	pr.preco_venda,
+	4
+FROM
+	produtos pr
+WHERE
+	pr.id = 4;
+
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Katya Petrolenko",
+	"R. Suíça, 406, Pinheiros - Sao Paulo, SP",
+	"+551196354311",
+	2,
+	pr.preco_venda,
+	9
+FROM
+	produtos pr
+WHERE
+	pr.id = 9;
+
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Мицухико Такущи",
+	"R. Frei Caneca, 640, Consolação - Sao Paulo, SP",
+	"+551145672211",
+	1,
+	pr.preco_venda,
+	10
+FROM
+	produtos pr
+WHERE
+	pr.id = 10;
+
+
+INSERT INTO
+	pedidos (
+		cliente_nome,
+		cliente_endereco,
+		cliente_telefone,
+		quantidade,
+		preco_unitario,
+		id_produto
+	)
+SELECT
+	"Dimitri Korzh",
+	"R. Russia, 416, Butanta - Sao Paulo, SP",
+	"+5511964311",
+	3,
+	pr.preco_venda,
+	5
+FROM
+	produtos pr
+WHERE
+	pr.id = 5;
+
 
 -- -- -- -- -- -- -- -- -- -- -- --
 -- -- -- -- -- -- -- -- -- -- -- --
-
-SELECT * 
-FROM fseletro.pedidos;
-
-SELECT 	pe.id Pedido, 
-				pe.cliente_nome Cliente, 
-				pe.id_produto CodProd, 
-				pr.descricao Item, 
-				pe.preco_unitario Preco, 
-				pe.quantidade Qtde, 
-				pe.preco_unitario*pe.quantidade Total
-FROM pedidos pe 
-JOIN produtos pr 
-ON pe.id_produto = pr.id;
+SELECT
+	*
+FROM
+	fseletro.pedidos;
 
 
-
-
+SELECT
+	pe.id Pedido,
+	pe.cliente_nome Cliente,
+	pe.id_produto CodProd,
+	pr.descricao Item,
+	pe.preco_unitario Preco,
+	pe.quantidade Qtde,
+	pe.preco_unitario * pe.quantidade Total
+FROM
+	pedidos pe
+	JOIN produtos pr ON pe.id_produto = pr.id;
--- 04 - Intro to DB (SQL)/queryProdutos.sql
+++ 04 - Intro to DB (SQL)/queryProdutos.sql
@@ -3,85 +3,284 @@
 -- DROP SCHEMA fseletro;
 -- DELETE FROM fseletro.produtos WHERE id = 3;
 -- -- DANGER ZONE -- --
-
 CREATE SCHEMA fseletro;
+
+
 USE fseletro;
 
+
 CREATE TABLE fseletro.produtos (
-	id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     data_inclusao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    categoria VARCHAR(100)  NOT NULL,
-    descricao VARCHAR(500)  NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    descricao VARCHAR(500) NOT NULL,
     estoque INT NOT NULL,
-    preco DECIMAL(8,2),
-    preco_venda DECIMAL(8,2),
+    preco DECIMAL(8, 2),
+    preco_venda DECIMAL(8, 2),
     imagem VARCHAR(100),
     PRIMARY KEY (id)
 );
 
+
 -- only for testing purposes
 -- ALTER TABLE fseletro.produtos
 -- ADD COLUMN estoque INT NOT NULL AFTER descricao;
-
 -- only for testing purposes
 -- ALTER TABLE fseletro.produtos
 -- ADD COLUMN data_inclusao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER id;
-
 -- only for testing purposes
 -- ALTER TABLE fseletro.produtos 
 -- ADD COLUMN Teste2 VARCHAR(45) AFTER imagem;
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "microwave",
+        "img/product1.png",
+        "Microondas 25L Espelhado Philco 220v",
+        1589.00,
+        1019.00,
+        35
+    );
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("microwave", "img/product1.png","Microondas 25L Espelhado Philco 220v", 1589.00, 1019.00, 35);
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("microwave", "img/product2.png","Forno de Microondas Eletrolux 20L Branco", 2039.00, 1819.00, 41);
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "microwave",
+        "img/product2.png",
+        "Forno de Microondas Eletrolux 20L Branco",
+        2039.00,
+        1819.00,
+        41
+    );
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("refrigerator", "img/product3.png","Geladeira Frost Free Brastemp Side Inverse 540 litros", 11380.00, 9099.00, 32);
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("refrigerator", "img/product4.png","Geladeira Frost Free Brastemp Branca 375 litros", 6389.00, 5019.00, 8);
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "refrigerator",
+        "img/product3.png",
+        "Geladeira Frost Free Brastemp Side Inverse 540 litros",
+        11380.00,
+        9099.00,
+        32
+    );
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("dishwasher", "img/product5.png","Lava Louça Compacta 8 Serviços Branca 127V Brastemp", 2389.00, 1719.00, 17);
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("dishwasher", "img/product6.png","Lava-Louças Electrolux Inox com 10 Serviços, 06 Programas de Lavagem e Painel Blue Touch", 4390.00, 2518.00, 45);
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "refrigerator",
+        "img/product4.png",
+        "Geladeira Frost Free Brastemp Branca 375 litros",
+        6389.00,
+        5019.00,
+        8
+    );
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("laundryMachine", "img/product7.png","Lavadora de Roupas Philco Inverter 12KG", 3309.00, 2190.00, 9);
 
-INSERT INTO fseletro.produtos (categoria, imagem, descricao, preco, preco_venda, estoque)
-VALUES("laundryMachine", "img/product8.png","Lavadora de Roupas Brastemp 11 kg com Turbo Performance Branca", 4018.00, 3235.00, 24);
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "dishwasher",
+        "img/product5.png",
+        "Lava Louça Compacta 8 Serviços Branca 127V Brastemp",
+        2389.00,
+        1719.00,
+        17
+    );
+
+
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "dishwasher",
+        "img/product6.png",
+        "Lava-Louças Electrolux Inox com 10 Serviços, 06 Programas de Lavagem e Painel Blue Touch",
+        4390.00,
+        2518.00,
+        45
+    );
+
+
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "laundryMachine",
+        "img/product7.png",
+        "Lavadora de Roupas Philco Inverter 12KG",
+        3309.00,
+        2190.00,
+        9
+    );
+
+
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+(
+        "laundryMachine",
+        "img/product8.png",
+        "Lavadora de Roupas Brastemp 11 kg com Turbo Performance Branca",
+        4018.00,
+        3235.00,
+        24
+    );
+
+
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+    (
+        "laundryMachine",
+        "img/product9.png",
+        "Samsung WW75J54E0IW 7.5kg Front Load Washing Machine",
+        3353.00,
+        2599.00,
+        10
+    );
+
+
+INSERT INTO
+    fseletro.produtos (
+        categoria,
+        imagem,
+        descricao,
+        preco,
+        preco_venda,
+        estoque
+    )
+VALUES
+    (
+        "laundryMachine",
+        "img/product10.png",
+        "AEG Washing Machine L7FE8432S",
+        3210.00,
+        2150.00,
+        8
+    );
+
 
 -- -- -- -- -- -- -- -- -- -- -- --
 -- -- -- -- -- -- -- -- -- -- -- --
+SELECT
+    *
+FROM
+    fseletro.produtos;
 
-SELECT * 
-FROM fseletro.produtos;
 
-SELECT * 
-FROM fseletro.produtos
-WHERE preco > 5000 AND preco > 10000;
+SELECT
+    *
+FROM
+    fseletro.produtos
+WHERE
+    preco > 5000
+    AND preco > 10000;
 
-SELECT * 
-FROM fseletro.produtos
-WHERE preco < 3000 OR preco > 10000;
 
-SELECT * 
-FROM fseletro.produtos
-WHERE categoria = 'microwave';
+SELECT
+    *
+FROM
+    fseletro.produtos
+WHERE
+    preco < 3000
+    OR preco > 10000;
 
-UPDATE fseletro.produtos
-SET preco = preco + 0.35 , preco_venda = preco_venda * 1.5
-WHERE id > 0;
 
-UPDATE fseletro.produtos
-SET preco = preco + 0.05 , preco_venda = preco * 0.2
-WHERE id > 0;
+SELECT
+    *
+FROM
+    fseletro.produtos
+WHERE
+    categoria = 'microwave';
 
-CREATE USER 'user3'@'localhost' IDENTIFIED 
-WITH mysql_native_password 
-BY 'pass123'
 
+UPDATE
+    fseletro.produtos
+SET
+    preco = preco + 0.35,
+    preco_venda = preco_venda * 1.5
+WHERE
+    id > 0;
+
+
+UPDATE
+    fseletro.produtos
+SET
+    preco = preco + 0.05,
+    preco_venda = preco * 0.2
+WHERE
+    id > 0;
+
+
+CREATE USER 'user3' @'localhost' IDENTIFIED WITH mysql_native_password BY 'pass123'
